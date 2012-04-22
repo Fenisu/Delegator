9d81ddd0542be91efbfb2aaf68f6d0d7ca5b63dd
Adam Johansson <luceatadam@gmail.com>
2012-04-22 23:02:44 +0200

    Added TimerActivity.java, Collaborator.java, timerview.xml, Clock.java

---
+++ src/com/delegator/Collaborator.java
@@ -0,0 +1,16 @@
+package com.delegator;
+
+import android.provider.ContactsContract.CommonDataKinds.Email;
+
+public class Collaborator {
+    public final String name;
+    public final String email;
+    public final String phoneNumber;
+    
+    public Collaborator(String name, String email, String phoneNumber){
+        this.name = name;     
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+
+}
